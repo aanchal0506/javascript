74e32953db2d6e765a7b95039151ec243e25f233
aanchal0506 <ladhaanchal@gmail.com>
2026-08-24 23:34:22 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" //treat all JS code as newer version
+
+
+//alert("hello")-> we are using node,js not brower
+
+
+//code readability should be high
+
+ let name="aanchal"
+ let age=18
+ let isLoggedIn=false
+
+
+ //number(2 power 53) 
+ //bigint
+ //string =>""
+ //boolean=> true/ false
+ //null =>standalone value (empty)
+ //undefined =>no value assigned 
+ // symbol =>uniqueness
+
+
+
+ //object
+
+ //console.log(typeof null)=>object
+ //console.log(typeof undefined)=>undefinied
+
+
+
+
