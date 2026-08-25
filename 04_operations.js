e02d95b2cf58542b49e05fe488d38d551664384f
aanchal0506 <ladhaanchal@gmail.com>
2026-08-25 23:15:17 +0530

    operation

---
+++ 04_operations.js
@@ -0,0 +1,41 @@
+let value=3
+let negV=-value
+console.log(negV)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+let str1="hello"
+let str2=" aanchal"
+let str3=str1+str2
+
+console.log(str3)
+
+console.log(1+"2")//12
+console.log("1"+2)//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+console.log(true)//true
+console.log(+true)//1
+//console.log(true+)//error
+
+
+let num1,num2,num3
+num1 = num2 = num3= 2 + 2
+let gameCounter=100
+let gameCounter1=109
+console.log(gameCounter++);//100
+console.log(++gameCounter1);//110
+console.log(gameCounter)//101
+console.log(gameCounter1)//110
+
+
+
+
+
+
+
