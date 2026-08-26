da41286c8a37241a4972ec36fb952322d80dec9d
aanchal0506 <ladhaanchal@gmail.com>
2026-08-26 12:17:15 +0530

    math

---
+++ 07_String.js
@@ -0,0 +1,42 @@
+const name="hitesh"
+const repoCount=50
+//console.log(name+ repoCount+" value"); outdates
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("ludo-game");//object in key value pair 
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());//stack memory 
+
+console.log(gameName.charAt(3));
+
+console.log(gameName.indexOf('t'));
+
+console.log(gameName.substring(0,4));
+
+console.log(gameName.slice(-8,4));
+
+console.log(gameName.trim());///remove spaces
+
+console.log(gameName.replace('u','a'))
+
+console.log(gameName.includes('ga'));
+
+console.log(gameName.split("-"))//array [ 'ludo', 'game' ]
+
+
+
+
+
+
+
+
+
+
+
+
